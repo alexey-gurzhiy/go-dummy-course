02e149cc29a1bd116111c481f334a0cf15cf029d
alexey-gurzhiy <109467117+alexey-gurzhiy@users.noreply.github.com>
2022-08-05 01:18:09 +0300

    HW3

---
+++ Lesson2/HW3/main.go
@@ -0,0 +1,13 @@
+// 3. С клавиатуры вводится трехзначное число. Выведите цифры, соответствующие количество сотен, десятков и единиц в этом числе.
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var number int
+	fmt.Print("Введите число: ")
+	fmt.Scanln(&number)
+	fmt.Printf("В вашем тексте \nСотен: %d \nДесятков: %d \nЕдиниц: %d", number/100, (number-number/100*100)/10, number-number/10*10)
+}
