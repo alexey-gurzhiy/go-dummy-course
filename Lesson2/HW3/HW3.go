77778857e84ecec3881732036e534d9c9ee80668
alexey-gurzhiy <109467117+alexey-gurzhiy@users.noreply.github.com>
2022-08-05 01:55:04 +0300

    Joined in one file

--- Lesson2/HW3/HW3.go
+++ Lesson2/HW3/HW3.go
@@ -1,11 +1,11 @@
 // 3. С клавиатуры вводится трехзначное число. Выведите цифры, соответствующие количество сотен, десятков и единиц в этом числе.
-package main
+package HW3
 
 import (
 	"fmt"
 )
 
-func main() {
+func HW3() {
 	var number int
 	fmt.Print("Введите число: ")
 	fmt.Scanln(&number)
---
+++ Lesson2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"./HW1"
+	"./HW2"
+	"./HW3"
+)
+
+func main() {
+	HW1.HW1()
+	fmt.Printf("\n\n")
+
+	HW2.HW2()
+	fmt.Printf("\n\n")
+
+	HW3.HW3()
+	fmt.Printf("\n\n")
+}
