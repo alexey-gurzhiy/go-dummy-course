26ad944e59b4af50f33a19aa41b4b68235da241e
alexey-gurzhiy <109467117+alexey-gurzhiy@users.noreply.github.com>
2022-09-05 18:53:35 +0300

    Updates

--- Lesson8/main.go
+++ Lesson8/main.go
@@ -16,6 +16,8 @@ package main
 import (
 	configuration "Lesson8/Configuration"
 	"fmt"
+	"os"
+
 )
 
 
@@ -29,5 +31,18 @@ func main() {
 	fmt.Printf("kafka_broker: 	%s \n", cfg.Kafka_broker)
 	fmt.Printf("some_app_id: 	%s \n", cfg.Some_app_id)
 	fmt.Printf("some_app_key: 	%s \n", cfg.Some_app_key)
+fmt.Println(" ")
+file,err := os.Open("go.mod")
+if err != nil {
+	panic(err)
+}
+
+data := make([]byte, 24)
+result, err := file.Read(data)
+if err != nil{
+	panic(err)
+}
+fmt.Println(result, string(data))
+fmt.Printf("file: %v\n", file)
 
 }
